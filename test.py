52c963ad8fc1d3b305447c4431d48c96dbff2ff4
William McKinnerney <git@williammck.net>
2019-07-05 19:13:34 -0500

    Remove old test file

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-from config import TOKEN
-
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-
-print(TOKEN)
-print(PROJECT_DIR)
-os.execl("C:/Windows/System32/cmd.exe", "/k",'"D:/Python36/python.exe "'+PROJECT_DIR+'/bot.py"')
