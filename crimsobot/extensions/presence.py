7f67d04d9d9a69ca6d62bac93fbe2e2b734a462c
William McKinnerney <git@williammck.net>
2019-07-07 22:52:11 -0500

    Slight refactor to PresenceChanger

--- crimsobot/extensions/presence.py
+++ crimsobot/extensions/presence.py
@@ -10,25 +10,25 @@ class PresenceChanger:
         self.bot = bot
         self.bot.loop.create_task(self.change_presence())
 
+        # status to scroll (about 25 characters recommended)
+        self.current_status = 'crimsoBOT is watching...'
+
     async def change_presence(self):
         """Slow scrolling effect for "Playing" status message."""
 
         await self.bot.wait_until_ready()
 
-        # status to scroll (about 25 characters recommended)
-        current_status = 'crimsoBOT is watching...'
         while not self.bot.is_closed():
-            current_status = self._reorder_text(current_status)
-            await self.bot.change_presence(activity=Game(current_status))
+            self.current_status = self._reorder_text(self.current_status)
+            await self.bot.change_presence(activity=Game(self.current_status))
             await asyncio.sleep(7)
 
     @staticmethod
-    def _reorder_text(string_in):
+    def _reorder_text(text: str) -> str:
         """Reorders a string. Called iteratively to give scrolling effect."""
 
-        string_out = string_in[1:] + string_in[0]
-        return string_out
+        return text[1:] + text[0]
 
 
-def setup(bot):
+def setup(bot: CrimsoBOT):
     PresenceChanger(bot)
