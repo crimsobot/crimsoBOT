61c34dd5a0a6976bcb02b6457ec9ec375b0143d7
keli5 <46039037+keli5@users.noreply.github.com>
2020-07-03 11:29:06 -0500

    Fix >tarot one error (#75)

--- crimsobot/utils/tarot.py
+++ crimsobot/utils/tarot.py
@@ -169,14 +169,12 @@ async def reading(spread: str) -> Tuple[Optional[io.BytesIO], List[Tuple[str, st
         position_legend = ['PAST', 'PRESENT', 'FUTURE', 'REASON', 'POTENTIAL']
 
     elif spread == 'one':
-        # a singular card on its own
-        # what is it? present?
         bg_size = (w, h)
         cards = await Deck.get_random_cards(1)
         position = [
             (0, 0)
         ]
-        position_legend = ['']  # just assuming once again
+        position_legend = ['\u200d']
 
     else:
         raise commands.BadArgument('Spread is invalid.')
