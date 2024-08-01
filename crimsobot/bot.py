397d3ca7129c62c778e40d3c0b5bbd7ccf995675
4njru <h.andrew.lassiter@gmail.com>
2024-08-01 14:53:32 -0400

    Wacky hidden ways to guess; ruleset created in yaml

--- crimsobot/bot.py
+++ crimsobot/bot.py
@@ -9,8 +9,17 @@ from config import ADMIN_USER_IDS, BANNED_GUILD_IDS, DM_LOG_CHANNEL_ID, LEARNER_
 from crimsobot import db
 from crimsobot.context import CrimsoContext
 from crimsobot.data.img import CAPTION_RULES, IMAGE_RULES
-from crimsobot.exceptions import (BadCaption, LocationNotFound, NoImageFound, NoMatchingTarotCard,
-                                  NotDirectMessage, StrictInputFailed, ZoomNotValid)
+from crimsobot.exceptions import (
+    BadCaption,
+    LocationNotFound,
+    NoImageFound,
+    NoMatchingTarotCard,
+    NotAnInteger,
+    NotDirectMessage,
+    OutOfBounds,
+    StrictInputFailed,
+    ZoomNotValid
+    )
 from crimsobot.help_command import PaginatedHelpCommand
 from crimsobot.models.ban import Ban
 from crimsobot.utils import markov as m, tools as c
@@ -153,6 +162,16 @@ class CrimsoBOT(commands.Bot):
                 error_type = '**not good with zoom**'
                 traceback_needed = False
                 msg_to_user = f'Zoom level **{error.original.zoom}** not good!'
+
+            if isinstance(error.original, NotAnInteger):
+                error_type = '**not good with math**'
+                traceback_needed = False
+                msg_to_user = f'**{error.original.guess}** is not an integer!'
+
+            if isinstance(error.original, OutOfBounds):
+                error_type = '**not good with math**'
+                traceback_needed = False
+                msg_to_user = f'**{error.original.guess}** is out of bounds!'
 
             if isinstance(error.original, NoImageFound):
                 error_type = '**NO IMAGE**'
